987bc0d9c9a129160994c9f5db767b023e6239f1
Rakul Rajgopu <rabrosrajgopu@gmail.com>
2022-06-09 22:45:05 +0200

    added items

--- products.js
+++ products.js
@@ -1,9 +1,52 @@
 const products = [
 {
     id:0,
-    name: "Product 1",
-    price: 20.00,
-    description: "test"
+    name: "Iphone 13 Silikon Case Kalkrosa",
+    price: 55.00,
+    description: "test",
+    imgsrc: "./img/case kalkrosa.jpg",
 
-}
-]
+},
+
+
+{
+    id:1,
+    name: "Iphone 13 Silikon Case Dunstblau",
+    price: 45.00,
+    description: "test",
+    imgsrc: "./img/case dunstblau.jpg",
+},
+
+{
+    id:2,
+    name: "Iphone 13 Silikon Case Kumquat",
+    price: 57.00,
+    description: "test",
+    imgsrc: "./img/case kumquat.jpg",
+},
+
+{
+    id:3,
+    name: "Iphone 13 Silikon Case RED",
+    price: 45.00,
+    description: "test",
+    imgSrc: "./img/case RED.jpg",
+},
+
+{
+    id:4,
+    name: "Iphone 13 Silikon Case Schwarz",
+    price: 40.00,
+    description: "test",
+    imgSrc: "./img/case schwarz.jpg",
+},
+
+{
+    id:5,
+    name: "Iphone 13 Silikon Case Wolkenblau",
+    price: 32.00,
+    description: "test",
+    imgSrc: "./img/case wolkenblau.jpg",
+},
+
+];
