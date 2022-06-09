73210f63b36cbb9677105e555e0575048bb7b38b
Rakul Rajgopu <rabrosrajgopu@gmail.com>
2022-06-09 23:12:45 +0200

    added renderfunction

--- app.js
+++ app.js
@@ -3,7 +3,17 @@
 const productselement = document.querySelector(".products");
 
 function renderProducts(){
-    products.forEach(() =>{
-        
-    })
+    products.forEach((product) => {
+        productselement.innerHTML += `    
+            <img src = " ${product.imgsrc}">
+            <h2>${product.name}</h2>
+            <h2><small></small>${product.price}</h2>
+            <p>
+                ${product.description}
+            </p>
+
+
+        `
+    });
 }
+renderProducts();
