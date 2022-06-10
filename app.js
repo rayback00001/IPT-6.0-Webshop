510c5bf7bec863aab7b93fad831c72c38fc69df5
Rakul Rajgopu <rabrosrajgopu@gmail.com>
2022-06-10 09:27:01 +0200

    updated layout

--- app.js
+++ app.js
@@ -5,20 +5,24 @@ const productselement = document.querySelector(".products");
 function renderProducts(){
     products.forEach((product) => {
         productselement.innerHTML += `    
-            
-            <div class="card">
-                <div class="card-img">
-                    <img src="${product.imgsrc}">
-                </div>
-                <div class="description">
-                  <h1>${product.name}</h1>
-                  <p><b>Preis:</b><span>${product.price}</span></p>
-                </div>
-                <div class="button-show" onclick="addToCart(${product.id})">
-                  <a href="" class="i-d-w">In den Warenkorb</a>
-                </div>
-            </div>
+        
+        
+        <div class = "card-container">
+         <div class="card">
+         <div class="card-img">
+            <img src="${product.imgsrc}">
+         </div>
+         <div class="description">
+          <h1>${product.name}</h1>
+          <p><b>Preis:</b><span>${product.price}</span></p>
+         </div>
+          <div class="button-show" onclick="addToCart(${product.id})">
+          <a href="" class="i-d-w">In den Warenkorb</a>
+        </div>
+        </div>
 
+     </div>
+            
 
         `
     });
